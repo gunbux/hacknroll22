8cdc6cfe237d73f949a0114afd395689ffa7146d
chun <lamchunyu00@gmail.com>
2022-01-07 21:33:47 +0800

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './components/Home'
+
+function App() {
+  return (
+    <>
+      <div>sup</div>
+      <Home/>
+    </>
+  )
+}
+
+export default App;
